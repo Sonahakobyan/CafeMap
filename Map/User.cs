1850a42540e07a44af7e8ff6b17dcf7de761844c
Hakobyan <Hakobyan@SONAPC>
2017-04-28 23:26:20 +0100

    Added check static method to User

--- Map/User.cs
+++ Map/User.cs
@@ -7,12 +7,31 @@ namespace Map
 {
     class User
     {
-        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
         private List<Cafe> cafes;
+        private static List<User> users = new List<User>();
+
+        public User (string username,string password)
+        {
+            Username = username;
+            Password = password;
+            users.Add(this);
+        }
 
         public void WriteReview(Review review)
         {
             review.Cafe.Reviews.Add(review);
+        }
+
+        public static bool Check(string username,string password)
+        {
+            foreach(User user in users)
+            {
+                if (user.Username == username && user.Password == password)
+                    return true;
+            }
+            return false;//heto?) de es heriq er hl@ or :D 
         }
     }
 }
